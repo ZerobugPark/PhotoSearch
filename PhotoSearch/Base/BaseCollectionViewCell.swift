d9373ead944fd79789512f6d54f6e34aab41547d
young <zerobugpark@gmail.com>
2025-01-17 19:40:59 +0900

    [Add] - BaseCollectionViewCell 추가
    [수정]
    폴더명 BaseView -> Base

---
+++ PhotoSearch/Base/BaseCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  BaseCollectionViewCell.swift
+//  PhotoSearch
+//
+//  Created by youngkyun park on 1/17/25.
+//
+
+import UIKit
+
+class BaseCollectionViewCell: UICollectionViewCell {
+    
+}
