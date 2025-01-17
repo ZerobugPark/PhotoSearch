48a615b1c24796503cde30fa96267b8796586a6d
young <zerobugpark@gmail.com>
2025-01-17 16:48:56 +0900

    [ADD] - Home View Controller - TabBarController
    [Update]
    - Scene Delegate -> 네비게이션 컨트롤러 연결

---
+++ PhotoSearch/MainView/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewController.swift
+//  PhotoSearch
+//
+//  Created by youngkyun park on 1/17/25.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    static let id = "HomeViewController"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#function)
+        view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ PhotoSearch/TapBarController/TabBarController.swift
@@ -0,0 +1,25 @@
+//
+//  TabBarController.swift
+//  PhotoSearch
+//
+//  Created by youngkyun park on 1/17/25.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    func configireTabBarController() {
+        let firstVC = HomeViewController(nibName: String?, bundle: <#T##Bundle?#>)
+        
+    }
+    
+    
+}
