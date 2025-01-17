8124fd2c52d470299f92e5fe98e6dc042150ccb1
young <zerobugpark@gmail.com>
2025-01-17 18:03:49 +0900

    [ADD] - CustomLabel 추가

---
+++ PhotoSearch/CustomObject/CustomLabel.swift
@@ -0,0 +1,24 @@
+//
+//  CustomLabel.swift
+//  PhotoSearch
+//
+//  Created by youngkyun park on 1/17/25.
+//
+
+import UIKit
+
+class CustomLabel: UILabel {
+    
+    
+    init(boldStyle: Bool, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        
+        font = boldStyle ? .boldSystemFont(ofSize: fontSize) : .systemFont(ofSize: fontSize)
+        
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
