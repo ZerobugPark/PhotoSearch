52d546abe0d1ee160c9cee3b33918e4021c45c23
young <zerobugpark@gmail.com>
2025-01-17 17:49:08 +0900

    [수정] - TabBar Controller에 ViewController 추가
    **** 주의사항
    - nibName은 xib 파일이 있을 때 사용

--- PhotoSearch/TapBarController/TabBarController.swift
+++ PhotoSearch/TapBarController/TabBarController.swift
@@ -11,15 +11,64 @@ class TabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print(#function)
         
+        configireTabBarController()
+        setupTabBarAppearance()
+        //configureApperance()
     }
     
 
     func configireTabBarController() {
-        let firstVC = HomeViewController(nibName: String?, bundle: <#T##Bundle?#>)
+        let firstVC = HomeViewController()
+        firstVC.tabBarItem.title = "첫화면"
+        firstVC.tabBarItem.image = UIImage(systemName: "star")
+        firstVC.tabBarItem.selectedImage = UIImage(systemName: "star.fil")
+        let firstNav = UINavigationController(rootViewController: firstVC)
+        
+        let secondVC = SerachViewController()
+        secondVC.tabBarItem.title = "두번째 화면"
+        secondVC.tabBarItem.image = UIImage(systemName: "trash")
+        secondVC.tabBarItem.selectedImage = UIImage(systemName: "trash.fil")
+        
+        setViewControllers([firstVC, secondVC], animated: true)
+    }
+    
+    
+    func setupTabBarAppearance() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.backgroundColor = .blue
+        tabBar.standardAppearance = appearance
+        tabBar.scrollEdgeAppearance = appearance
+        tabBar.tintColor = .black
+
+    }
+    
+    func configureApperance() {
+        
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.configureWithOpaqueBackground()
+        navigationBarAppearance.backgroundColor = .red
+        navigationBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navigationBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        
+        
+        let tabBarApperance = UITabBarAppearance()
+        tabBarApperance.configureWithOpaqueBackground()
+        tabBarApperance.backgroundColor = .white
+        
         
     }
     
     
+}
+
+
+extension TabBarController: UITabBarControllerDelegate {
+    
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        print(item)
+    }
 }
