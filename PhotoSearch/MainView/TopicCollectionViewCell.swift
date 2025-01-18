b9ce550f1b374732d469968c0305073c38698ffe
young <zerobugpark@gmail.com>
2025-01-18 15:15:11 +0900

    [ADD] Like 버튼 추가

--- PhotoSearch/MainView/TopicCollectionViewCell.swift
+++ PhotoSearch/MainView/TopicCollectionViewCell.swift
@@ -14,12 +14,13 @@ class TopicCollectionViewCell: BaseCollectionViewCell {
     static let id = "TopicCollectionViewCell"
     
     let imageView = CustomImageView()
- 
+    let likeButton = CustomButton(imgName: "star.fill")
 
+    
     override func configureHierarchy() {
         
         contentView.addSubview(imageView)
-        
+        contentView.addSubview(likeButton)
     }
     
     
@@ -29,12 +30,16 @@ class TopicCollectionViewCell: BaseCollectionViewCell {
             make.size.equalTo(contentView)
             
         }
+        likeButton.snp.makeConstraints { make in
+            make.leading.equalTo(contentView.safeAreaLayoutGuide).offset(8)
+            make.bottom.equalTo(contentView.safeAreaLayoutGuide).inset(8)
+            make.width.greaterThanOrEqualTo(20)
+        }
 
     }
     
     override func configureView() {
     
-        //imageView.backgroundColor = .red
     }
     
     
@@ -42,6 +47,8 @@ class TopicCollectionViewCell: BaseCollectionViewCell {
         
         let url = URL(string: data.urls.small)
         imageView.kf.setImage(with: url)
+        
+        likeButton.setTitle(data.likes.formatted(), for: .normal)
     }
     
 }
