729c6f507488b364428fd4ecbd2d37864151e75a
young <zerobugpark@gmail.com>
2025-01-19 18:30:20 +0900

    [기능 추가]
    - 사진 상세정보에 관련된 유저 Detail Api 추가

--- PhotoSearch/NetworkManger.swift
+++ PhotoSearch/NetworkManger.swift
@@ -16,6 +16,8 @@ class NetworkManager {
     private init() { }
     
 
+    
+    // 기능은 비슷하나 매개변수가 다른 경우, 오버로딩이 나을까요? 아니면, 함수명을 조금 더 직관적으로 설정하는게 나을까요?.
     func callRequestTopic(completionHandler: @escaping ([TopicList]) -> Void) {
         let url = "https://api.unsplash.com/topics?client_id=\(UnSplashAPI.client_ID)&per_page=30"
         
@@ -34,7 +36,7 @@ class NetworkManager {
     
     func callRequestTopicImage(topic: String, completionHandler: @escaping ([UnslpashTopic]) -> Void) {
         
-        let url = "https://api.unsplash.com/topics/\(topic)/photos/?&client_id=\(UnSplashAPI.client_ID)&order_by=latest"
+        let url = "https://api.unsplash.com/topics/\(topic)/photos/?client_id=\(UnSplashAPI.client_ID)&order_by=latest"
         
         AF.request(url, method: .get).responseString { value in
             //print(value)
@@ -55,9 +57,9 @@ class NetworkManager {
     
 
     func callRequestGetImage(query: String, filter: String, page: Int ,completionHandler: @escaping (UnslpashGetImage) -> Void) {
-        let url =     "https://api.unsplash.com/search/photos?query=\(query)&order_by=\(filter)&client_id=\(UnSplashAPI.client_ID)&page=\(page)&per_page=20"
+        let url = "https://api.unsplash.com/search/photos?client_id=\(UnSplashAPI.client_ID)&query=\(query)&order_by=\(filter)&page=\(page)&per_page=20"
      
-        print(url)
+        //print(url)
         AF.request(url, method: .get).validate(statusCode: 0..<300).responseDecodable(of: UnslpashGetImage.self) { response in
             //print(response.response?.statusCode)
             
@@ -72,7 +74,7 @@ class NetworkManager {
     }
     
     func callRequestGetImageWithColor(query: String, filter: String, page: Int,color: String ,completionHandler: @escaping (UnslpashGetImage) -> Void) {
-        let url =     "https://api.unsplash.com/search/photos?&client_id=\(UnSplashAPI.client_ID)&query=\(query)&order_by=\(filter)&page=\(page)&per_page=20&color=\(color)"
+        let url = "https://api.unsplash.com/search/photos?&client_id=\(UnSplashAPI.client_ID)&query=\(query)&order_by=\(filter)&page=\(page)&per_page=20&color=\(color)"
      
         print(url)
         AF.request(url, method: .get).validate(statusCode: 0..<300).responseDecodable(of: UnslpashGetImage.self) { response in
@@ -88,7 +90,23 @@ class NetworkManager {
         }
     }
     
-
+    func callRequestGetUserDetail(id: String, completionHandler: @escaping (UserDetail) -> Void) {
+        let url = "https://api.unsplash.com/photos/\(id)/statistics?client_id=\(UnSplashAPI.client_ID)"
+        
+        print(url)
+        AF.request(url, method: .get).validate(statusCode: 0..<300).responseDecodable(of: UserDetail.self) { response in
+            //print(response.response?.statusCode)
+            
+            switch response.result {
+            case .success(let value):
+                //dump(value)
+                completionHandler(value)
+            case.failure(let error):
+                dump(error)
+            }
+        }
+    }
+    
 
     
     
