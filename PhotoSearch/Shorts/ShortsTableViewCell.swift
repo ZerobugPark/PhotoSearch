7e5fb230a9d0151dceab36f6e31f15e3e4dca7f1
young <zerobugpark@gmail.com>
2025-01-21 21:46:39 +0900

    [파일추가] -shorts 뷰컨트롤러 및 테이블셀 추가

---
+++ PhotoSearch/Shorts/ShortsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ShortsTableViewCell.swift
+//  PhotoSearch
+//
+//  Created by youngkyun park on 1/21/25.
+//
+
+import UIKit
+
+class ShortsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
