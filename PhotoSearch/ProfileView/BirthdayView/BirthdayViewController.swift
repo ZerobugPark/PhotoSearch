033ff08790e1420cab2af700bf8df5f390b7119a
young <zerobugpark@gmail.com>
2025-01-23 17:36:24 +0900

    [추가]
    - Profile detail setting view 추가 (birthday, level, nickname)

---
+++ PhotoSearch/ProfileView/BirthdayView/BirthdayViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BirthdayViewController.swift
+//  PhotoSearch
+//
+//  Created by youngkyun park on 1/23/25.
+//
+
+import UIKit
+
+class BirthdayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+
+}
